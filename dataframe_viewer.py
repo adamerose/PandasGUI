6279dcb9cf597f9aa16ff5b1e558c0d4ce1ffccb
Adam Rose <adrotog@gmail.com>
2018-12-05 03:46:32 -0500

    moved dataframeviewer class to another file for modularity attempt 2

--- dataframe_viewer.py
+++ dataframe_viewer.py
@@ -5,18 +5,14 @@ import sys
 # This fixes lack of stack trace on PyQt exceptions
 import pyqt_fix
 
-class DataFrameView(QtWidgets.QTableView):
-    def __init__(self, parent=None):
-        super().__init__(parent=parent)
-
-        self.verticalHeader().hide()
-
-
 
 class DataFrameModel(QtCore.QAbstractTableModel):
     def __init__(self, df=pd.DataFrame(), parent=None):
         super().__init__(parent=parent)
         self.df = df
+
+    def __str__(self):
+        return str(self.df)
 
     def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
         if role == QtCore.Qt.DisplayRole:
@@ -33,6 +29,9 @@ class DataFrameModel(QtCore.QAbstractTableModel):
     def rowCount(self, parent=QtCore.QModelIndex()):
         return len(self.df.index)
 
+    def __len__(self):
+        return self.rowCount()
+
     def columnCount(self, parent=QtCore.QModelIndex()):
         return len(self.df.columns)
 
@@ -44,11 +43,13 @@ class DataFrameModel(QtCore.QAbstractTableModel):
         self.layoutChanged.emit()
 
     def flags(self, index):
-        ''' Set flag to allow items editable (and enabled / selectable but those are on by default'''
+        ''' Set flag to allow items editable
+        (and enabled / selectable but those are on by default)'''
         return QtCore.Qt.ItemIsEditable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
 
     def setData(self, index, value, role=QtCore.Qt.EditRole):
-        ''' When an item is edited check if it is valid, if it is, return True and emit dataChanged'''
+        ''' When an item is edited check if it is valid,
+            if it is, return True and emit dataChanged'''
         if role == QtCore.Qt.EditRole:
             row = index.row()
             column = index.column()
@@ -56,19 +57,3 @@ class DataFrameModel(QtCore.QAbstractTableModel):
             self.df.iloc[row, column] = value
             self.dataChanged.emit(index, index)
             return True
-
-if __name__ == '__main__':
-
-    df = pd.DataFrame([["11", "12", "13"], ["21", "22", "23"], ["31", "32", "33"]], columns=["A", "B", "C"])
-
-
-
-    app = QtWidgets.QApplication(sys.argv)
-
-    model = DataFrameModel(df)
-    view = DataFrameView()
-    view.setModel(model)
-    view.setFixedSize(800,600)
-    view.show()
-
-    app.exec_()
--- pdgui.py
+++ pdgui.py
@@ -4,6 +4,7 @@ import sys
 
 # This fixes lack of stack trace on PyQt exceptions
 import pyqt_fix
+from dataframe_viewer import DataFrameModel
 
 
 class PandasGUI(QtWidgets.QMainWindow):
@@ -157,59 +158,6 @@ class PandasGUI(QtWidgets.QMainWindow):
     def clear_layout(self, layout):
         for i in reversed(range(layout.count())):
             layout.itemAt(i).widget().setParent(None)
-
-
-class DataFrameModel(QtCore.QAbstractTableModel):
-    def __init__(self, df=pd.DataFrame(), parent=None):
-        super().__init__(parent=parent)
-        self.df = df
-
-    def __str__(self):
-        return str(self.df)
-
-    def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
-        if role == QtCore.Qt.DisplayRole:
-            if orientation == QtCore.Qt.Horizontal:
-                return self.df.columns.tolist()[section]
-            elif orientation == QtCore.Qt.Vertical:
-                return self.df.index.tolist()[section]
-
-    def data(self, index, role=QtCore.Qt.DisplayRole):
-        if (role == QtCore.Qt.DisplayRole) | (role == QtCore.Qt.EditRole) | (role == QtCore.Qt.ToolTipRole):
-            if index.isValid():
-                return str(self.df.iloc[index.row(), index.column()])
-
-    def rowCount(self, parent=QtCore.QModelIndex()):
-        return len(self.df.index)
-
-    def __len__(self):
-        return self.rowCount()
-
-    def columnCount(self, parent=QtCore.QModelIndex()):
-        return len(self.df.columns)
-
-    def sort(self, column, order):
-        colname = self.df.columns.tolist()[column]
-        self.layoutAboutToBeChanged.emit()
-        self.df.sort_values(colname, ascending=order == QtCore.Qt.AscendingOrder, inplace=True)
-        self.df.reset_index(inplace=True, drop=True)
-        self.layoutChanged.emit()
-
-    def flags(self, index):
-        ''' Set flag to allow items editable
-        (and enabled / selectable but those are on by default)'''
-        return QtCore.Qt.ItemIsEditable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
-
-    def setData(self, index, value, role=QtCore.Qt.EditRole):
-        ''' When an item is edited check if it is valid,
-            if it is, return True and emit dataChanged'''
-        if role == QtCore.Qt.EditRole:
-            row = index.row()
-            column = index.column()
-
-            self.df.iloc[row, column] = value
-            self.dataChanged.emit(index, index)
-            return True
 
 
 def show(df):
