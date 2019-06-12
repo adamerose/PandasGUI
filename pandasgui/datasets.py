e28e064a013acfbe24f61ec8147e9b058963727a
Adam Rose <adrotog@gmail.com>
2019-06-12 13:22:08 -0400

    Added all_datasets to export

--- pandasgui/datasets.py
+++ pandasgui/datasets.py
@@ -1,4 +1,4 @@
-__all__ = ['iris','flights','multi','pokemon']
+__all__ = ['iris','flights','multi','pokemon','all_datasets']
 
 import seaborn as sns
 import pandas as pd
@@ -11,3 +11,11 @@ iris = sns.load_dataset('iris')
 flights = sns.load_dataset('flights')
 multi = flights.set_index(['year', 'month']).unstack()  # MultiIndex example
 pokemon = pd.read_csv(data_path+"pokemon.csv")
+
+
+# All data sets
+all_datasets = {}
+for name in sns.get_dataset_names():
+    all_datasets[name] = sns.load_dataset(name)
+
+all_datasets['pokemon'] = pokemon
