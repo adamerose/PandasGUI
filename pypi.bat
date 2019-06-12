2dc97012f7cfa94b25c1be9132580b327694e5b0
Adam Rose <adrotog@gmail.com>
2019-06-12 12:36:09 -0400

    Temporarily removed docs

--- pypi.bat
+++
@@ -1,9 +0,0 @@
-rmdir build /Q/S
-rmdir dist /Q/S
-rmdir include /Q/S
-rmdir Lib /Q/S
-rmdir tcl /Q/S
-rmdir pandasgui.egg-info /Q/S
-python setup.py bdist_wheel sdist
-python3 -m twine upload dist/*
-PAUSE
