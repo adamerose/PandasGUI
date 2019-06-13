19e51c3c6d9f7d77d6ffb2057c65bc7283084616
Adam Rose <adrotog@gmail.com>
2019-06-13 10:40:26 -0400

    Moved demo to root folder

---
+++ demo.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import seaborn as sns
+from pandasgui import show
+
+import warnings
+# warnings.filterwarnings('error')
+
+case = 1
+
+if case == 1:
+    # Sample data sets
+    from pandasgui.datasets import iris, flights, multi, pokemon
+
+    show(iris, flights, multi)
+
+if case == 2:
+    # View all Seaborn data sets
+    datasets = {}
+    for name in sns.get_dataset_names():
+        datasets[name] = sns.load_dataset(name)
+
+    show(**datasets)
--- pandasgui/datasets.py
+++ pandasgui/datasets.py
@@ -2,19 +2,18 @@ __all__ = ['iris','flights','multi','pokemon','all_datasets']
 
 import seaborn as sns
 import pandas as pd
-import pkg_resources
-
-data_path = pkg_resources.resource_filename(__name__, 'data/')
-
+import os
+cwd = os.path.dirname(__file__)
 
 iris = sns.load_dataset('iris')
 flights = sns.load_dataset('flights')
 multi = flights.set_index(['year', 'month']).unstack()  # MultiIndex example
-pokemon = pd.read_csv(data_path+"pokemon.csv")
+pokemon = pd.read_csv(os.path.join(cwd,"data/pokemon.csv"))
 
-
-# All data sets
 all_datasets = {}
+
+
+# All Seaborn data sets
 for name in sns.get_dataset_names():
     all_datasets[name] = sns.load_dataset(name)
 
