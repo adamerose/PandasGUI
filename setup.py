50aedf8e20ba6d38781a5841f899e6bd675dd3b9
Adam Rose <adrotog@gmail.com>
2019-06-14 00:09:27 -0400

    Don't like having description duplicated here and in readme on PyPi

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(name='pandasgui',
-      version='0.1.0',
-      description='A GUI for viewing and analyzing Pandas DataFrames based on PyQt5.',
+      version='0.1.1',
       author='Adam Rose',
       author_email='adam.e.rose@hotmail.com',
       url='https://github.com/adamerose/pandasgui',
