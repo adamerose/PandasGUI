28f5e7afe2a5b6178f312dba6c01a84ee111d465
Adam Rose <adrotog@gmail.com>
2019-06-13 23:19:33 -0400

    Changed datasets

--- pandasgui/datasets.py
+++ pandasgui/datasets.py
@@ -1,4 +1,4 @@
-__all__ = ['iris','flights','multi','pokemon','all_datasets']
+__all__ = ['iris','flights','multi','pokemon','multidf','all_datasets']
 
 import seaborn as sns
 import pandas as pd
@@ -10,6 +10,11 @@ flights = sns.load_dataset('flights')
 multi = flights.set_index(['year', 'month']).unstack()  # MultiIndex example
 pokemon = pd.read_csv(os.path.join(cwd,"data/pokemon.csv"))
 
+tuples = [('A', 'one', 'x'), ('A', 'one', 'y'), ('A', 'two', 'x'), ('A', 'two', 'y'),
+                  ('B', 'one', 'x'), ('B', 'one', 'y'), ('B', 'two', 'x'), ('B', 'two', 'y')]
+index = pd.MultiIndex.from_tuples(tuples, names=['first', 'second', 'third'])
+multidf = pd.DataFrame(pd.np.random.randn(8, 8), index=index[:8], columns=index[:8])
+
 all_datasets = {}
 
 
@@ -18,3 +23,4 @@ for name in sns.get_dataset_names():
     all_datasets[name] = sns.load_dataset(name)
 
 all_datasets['pokemon'] = pokemon
+all_datasets['multidf'] = multidf
