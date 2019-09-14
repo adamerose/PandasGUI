a324270014a0967912e1534d15c12e7929554202
Sayon Bhattacharjee <sbsayon@gmail.com>
2019-09-14 15:47:26 -0400

    organization

--- pandasgui/widgets/__init__.py
+++ pandasgui/widgets/__init__.py
@@ -4,10 +4,9 @@ __all__ = ['DataFrameExplorer', 'DataFrameViewer', 'FigureViewer',
            'TabbedFigureViewer', 'PivotDialog', 'ScatterDialog',
            'Find_Toolbar']
 
+from pandasgui.widgets.find_toolbar import Find_Toolbar
 from pandasgui.widgets.image_viewer import FigureViewer, TabbedFigureViewer
 from pandasgui.widgets.dialogs import PivotDialog, ScatterDialog
 from pandasgui.widgets.dataframe_viewer import DataFrameViewer
 
 from pandasgui.widgets.dataframe_explorer import DataFrameExplorer
-
-from pandasgui.widgets.find_toolbar import Find_Toolbar
