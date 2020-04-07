59801d9f500e820e3fc1fcf28c59bda7b728c373
Adam Rose <adrotog@gmail.com>
2020-04-07 06:33:30 -0400

    Continue without tabs if fail to create

--- pandasgui/widgets/dataframe_explorer.py
+++ pandasgui/widgets/dataframe_explorer.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from pandasgui.widgets import DataFrameViewer
 from pandasgui.widgets import FigureViewer
+import traceback
 
 class DataFrameExplorer(QtWidgets.QTabWidget):
     """
@@ -28,13 +29,19 @@ class DataFrameExplorer(QtWidgets.QTabWidget):
         self.addTab(self.dataframe_tab, "DataFrame")
 
         # Statistics tab
-        self.statistics_tab = self.make_statistics_tab(df)
-        self.addTab(self.statistics_tab, "Statistics")
+        try:
+            self.statistics_tab = self.make_statistics_tab(df)
+            self.addTab(self.statistics_tab, "Statistics")
+        except:
+            traceback.print_exc()
 
         # Histogram tab
-        if not (type(df.index) == pd.MultiIndex or type(df.columns) == pd.MultiIndex):
-            histogram_tab = self.make_histogram_tab(df)
-            self.addTab(histogram_tab, "Histogram")
+        try:
+            if not (type(df.index) == pd.MultiIndex or type(df.columns) == pd.MultiIndex):
+                histogram_tab = self.make_histogram_tab(df)
+                self.addTab(histogram_tab, "Histogram")
+        except:
+            traceback.print_exc()
 
     def make_statistics_tab(self, df):
         stats_df = pd.DataFrame({
