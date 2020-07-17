dca92fe74b77371fc162f2eb115df6a36b615d2a
Adam Rose <adrotog@gmail.com>
2020-07-17 12:14:31 -0400

    Refactoring

--- pandasgui/widgets/__init__.py
+++ pandasgui/widgets/__init__.py
@@ -1,15 +1,28 @@
-"""Reusable PyQt widgets"""
+__all__ = [
+    "QtWaitingSpinner",
+    "FindToolbar",
+    "FigureViewer",
+    "TabbedFigureViewer",
+    "PivotDialog",
+    "ScatterDialog",
+    "PlotlyViewer",
+    "GraphBuilder",
+    "DataFrameViewer",
+    "DataFrameExplorer",
+]
 
-__all__ = ['DataFrameExplorer', 'DataFrameViewer', 'FigureViewer', 'TabbedFigureViewer', 'PivotDialog',
-           'ScatterDialog', 'PlotlyViewer', 'GraphBuilder', 'FindToolbar', 'QtWaitingSpinner']
-
+# Basic widgets
 from pandasgui.widgets.spinner import QtWaitingSpinner
 from pandasgui.widgets.find_toolbar import FindToolbar
-from pandasgui.widgets.image_viewer import FigureViewer, TabbedFigureViewer
+
+from pandasgui.widgets.image_viewer import FigureViewer
+from pandasgui.widgets.image_viewer import TabbedFigureViewer
+
+from pandasgui.widgets.dialogs import PivotDialog
+from pandasgui.widgets.dialogs import ScatterDialog
+
 from pandasgui.widgets.plotly_viewer import PlotlyViewer
-from pandasgui.widgets.dialogs import PivotDialog, ScatterDialog
 from pandasgui.widgets.graph_builder import GraphBuilder
 
 from pandasgui.widgets.dataframe_viewer import DataFrameViewer
 from pandasgui.widgets.dataframe_explorer import DataFrameExplorer
-
