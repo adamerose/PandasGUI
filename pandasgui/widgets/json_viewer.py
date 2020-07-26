5739ebc0c23f20a3aeb167744044f82c302304c6
Adam Rose <adrotog@gmail.com>
2020-07-26 10:10:54 -0400

    Rewrite json viewer, and add it to GUI debug menu

--- pandasgui/widgets/json_viewer.py
+++ pandasgui/widgets/json_viewer.py
@@ -1,144 +1,98 @@
-# Based on https://github.com/ashwin/json-viewer
-
 import collections
 import json
 import sys
-
+from typing import Union
 from PyQt5 import QtCore, QtGui, QtWidgets
-
-
-class TextToTreeItem:
-
-    def __init__(self):
-        self.text_list = []
-        self.titem_list = []
-
-    def append(self, text_list, titem):
-        for text in text_list:
-            self.text_list.append(text)
-            self.titem_list.append(titem)
-
-    # Return model indices that match string
-    def find(self, find_str):
-
-        titem_list = []
-        for i, s in enumerate(self.text_list):
-            if find_str in s:
-                titem_list.append(self.titem_list[i])
-
-        return titem_list
 
 
 class JsonViewer(QtWidgets.QWidget):
 
-    def __init__(self, jdata):
-        super(JsonViewer, self).__init__()
+    def __init__(self, jdata: Union[list, dict], parent=None):
+        super().__init__(parent)
 
-        self.find_box = None
-        self.tree_widget = None
-        self.text_to_titem = TextToTreeItem()
-        self.find_str = ""
-        self.found_titem_list = []
-        self.found_idx = 0
-
-        # Find UI
-        find_layout = self.make_find_ui()
-
-        # Tree
+        self.find_box = QtWidgets.QLineEdit()
+        self.find_box.returnPressed.connect(self.find)
+        self.find_box.textChanged.connect(self.find)
+        self.find_box.setPlaceholderText("Find")
 
         self.tree_widget = QtWidgets.QTreeWidget()
         self.tree_widget.setHeaderLabels(["Key", "Value"])
         self.tree_widget.header().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
 
-        root_item = QtWidgets.QTreeWidgetItem(["Root"])
+        root_item = self.tree_widget.invisibleRootItem()
         self.recurse_jdata(jdata, root_item)
         self.tree_widget.addTopLevelItem(root_item)
+        self.tree_widget.expandAll()
 
-        # Add table to layout
+        self.expand_all = QtWidgets.QPushButton("Expand All")
+        self.expand_all.clicked.connect(self.tree_widget.expandAll)
+        self.collapse_all = QtWidgets.QPushButton("Collapse All")
+        self.collapse_all.clicked.connect(self.tree_widget.collapseAll)
 
-        layout = QtWidgets.QHBoxLayout()
+        top_section = QtWidgets.QHBoxLayout()
+        top_section.addWidget(self.find_box)
+        top_section.addWidget(self.expand_all)
+        top_section.addWidget(self.collapse_all)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addLayout(top_section)
         layout.addWidget(self.tree_widget)
 
-        # Group box
+        self.setLayout(layout)
 
-        gbox = QtWidgets.QGroupBox()
-        gbox.setLayout(layout)
-
-        layout2 = QtWidgets.QVBoxLayout()
-        layout2.addLayout(find_layout)
-        layout2.addWidget(gbox)
-
-        self.setLayout(layout2)
+        self.resize(QtCore.QSize(400,500))
 
         self.show()
 
-    def make_find_ui(self):
 
-        # Text box
-        self.find_box = QtWidgets.QLineEdit()
-        self.find_box.returnPressed.connect(self.find_button_clicked)
+    def find(self):
 
-        # Find Button
-        find_button = QtWidgets.QPushButton("Find")
-        find_button.clicked.connect(self.find_button_clicked)
+        text = self.find_box.text()
 
-        layout = QtWidgets.QHBoxLayout()
-        layout.addWidget(self.find_box)
-        layout.addWidget(find_button)
-
-        return layout
-
-    def find_button_clicked(self):
-
-        find_str = self.find_box.text()
-
-        # Very common for use to click Find on empty string
-        if find_str == "":
+        if text == "":
+            self.tree_widget.clearSelection()
             return
 
-        # New search string
-        if find_str != self.find_str:
-            self.find_str = find_str
-            self.found_titem_list = self.text_to_titem.find(self.find_str)
-            self.found_idx = 0
-        else:
-            item_num = len(self.found_titem_list)
-            self.found_idx = (self.found_idx + 1) % item_num
+        result = []
+        for col in [0,1]:
+            result += self.tree_widget.findItems(text,
+                                                 QtCore.Qt.MatchRegularExpression | QtCore.Qt.MatchRecursive,
+                                                 col)
+        self.tree_widget.clearSelection()
+        self.tree_widget.setSelectionMode(self.tree_widget.MultiSelection)
 
-        self.tree_widget.setCurrentItem(self.found_titem_list[self.found_idx])
+        for item in result:
+            item.setSelected(True)
+
+        self.tree_widget.setSelectionMode(self.tree_widget.ExtendedSelection)
 
     def recurse_jdata(self, jdata, tree_widget):
 
         if isinstance(jdata, dict):
-            for key, val in jdata.items():
-                self.tree_add_row(key, val, tree_widget)
+            items = jdata.items()
         elif isinstance(jdata, list):
-            for i, val in enumerate(jdata):
-                key = str(i)
-                self.tree_add_row(key, val, tree_widget)
+            items = [(str(i), val) for i, val in enumerate(jdata)]
         else:
-            print("This should never be reached!")
+            raise ValueError(f"Expected dict or list, instead got {type(jdata)}")
 
-    def tree_add_row(self, key, val, tree_widget):
+        for key, val in items:
+            text_list = []
 
-        text_list = []
+            if isinstance(val, dict) or isinstance(val, list):
+                text_list.append(key)
+                row_item = QtWidgets.QTreeWidgetItem([key])
+                self.recurse_jdata(val, row_item)
+            else:
+                text_list.append(key)
+                text_list.append(str(val))
+                row_item = QtWidgets.QTreeWidgetItem([key, str(val)])
 
-        if isinstance(val, dict) or isinstance(val, list):
-            text_list.append(key)
-            row_item = QtWidgets.QTreeWidgetItem([key])
-            self.recurse_jdata(val, row_item)
-        else:
-            text_list.append(key)
-            text_list.append(str(val))
-            row_item = QtWidgets.QTreeWidgetItem([key, str(val)])
-
-        tree_widget.addChild(row_item)
-        self.text_to_titem.append(text_list, row_item)
+            tree_widget.addChild(row_item)
 
 
 if "__main__" == __name__:
     qt_app = QtWidgets.QApplication(sys.argv)
-    data = [{
+    data_list = [{
         "name": "Tim",
         "age": 22,
         "cars": {
@@ -153,5 +107,14 @@ if "__main__" == __name__:
             "car3": "Fiat"
         }
     }]
-    json_viewer = JsonViewer(data)
+
+    data_dict = {
+        "name": "Tim",
+        "age": 22,
+        "cars": {
+            "car1": "Mazda",
+        }
+    }
+    json_viewer = JsonViewer(data_list)
+    json_viewer2 = JsonViewer(data_dict)
     sys.exit(qt_app.exec_())
