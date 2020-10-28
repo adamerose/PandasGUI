a7ea617785504ccaaecf3258f527cf731aca3986
Adam Rose <adrotog@gmail.com>
2020-10-28 16:04:30 -0400

    Switch to setuptools-git

--- setup.py
+++ setup.py
@@ -14,9 +14,13 @@ setup(
     include_package_data=True,
     long_description=long_description,
     long_description_content_type="text/markdown",
+    exclude_package_data={'': ['.gitignore']},
+    # Using this instead of MANIFEST.in - https://pypi.org/project/setuptools-git/
+    setup_requires=['setuptools-git'],
     install_requires=[
         "pandas",
         "PyQt5",
+        "PyQt5-sip",
         "PyQtWebEngine",
         "plotly",
         "setuptools",
