8fd12cc1b4c53c7243adfeabcb6d0971b96aad54
Francois Dion <fdion@dionresearch.com>
2021-02-01 22:24:59 -0500

    sorting on 7 digits (space) wide

--- pandasgui/widgets/grapher.py
+++ pandasgui/widgets/grapher.py
@@ -57,7 +57,7 @@ class Grapher(QtWidgets.QWidget):
 
         df = flatten_df(self.pgdf.df)
         self.dragger = Dragger(sources=df.columns, destinations=[],
-                               source_nunique=df.nunique().apply('{: >6}'.format).values,
+                               source_nunique=df.nunique().apply('{: >7}'.format).values,
                                source_types=df.dtypes.values.astype(str))
 
         self.layout = QtWidgets.QGridLayout()
