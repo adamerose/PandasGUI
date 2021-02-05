11cd13b95755ac78cab17f4dc590ceaf746f8d94
Francois Dion <fdion@dionresearch.com>
2021-02-05 15:39:59 -0500

    arbitrary cardinality of categoricals set at 50

--- pandasgui/constants.py
+++ pandasgui/constants.py
@@ -1,6 +1,8 @@
 import os
 from appdirs import user_data_dir
 
+CATEGORICAL_THRESHOLD = 50
+
 LOCAL_DATA_DIR = os.path.join(user_data_dir(), "pandasgui")
 LOCAL_DATASET_DIR = os.path.join(LOCAL_DATA_DIR, "dataset_files")
 
